6543ae3f9ed4abefbebc24ef0e7f52aff612dade
amaranth <81465068+amaran-th@users.noreply.github.com>
2024-09-08 21:59:05 +0900

    Update data.js

--- every_week_posting_challenge/data.js
+++ every_week_posting_challenge/data.js
@@ -1,21 +1,9 @@
 exports.members = {
-  lcqff: '임연후',
-  publdaze: '김은지',
-  llddang: '이다은',
-  chairoi: '신채원',
-  pnudpwls1103: '민예진',
-  shkisme: '손현경',
-  pipisebastian: '장서윤',
   'amaran-th': '송세연',
-  '02ggang9': '이수빈',
   '1jeongg': '남원정',
-  KwonNaeHyeon: '권내현',
-  'sososo0':'박소현',
   'stopmin':'정지민',
-  'mun-kyeong':'김문경',
   'gusah009':'정현모',
   'jojaeng2':'조정현',
-  'hyun98':'조현우',
   'pykido':'김태윤',
 }
 
